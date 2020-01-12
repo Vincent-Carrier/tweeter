3f11f36c4d4420516709f1b181d713c558dfdf01
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-12 17:29:00 -0500

    Format with Prettier

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,89 +3,97 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const daysAgo = (millis) => {
+const daysAgo = millis => {
   const now = new Date();
   const then = new Date(millis);
   const diff = now - then;
   return Math.floor(diff / 8.64e7);
-}
+};
 
-const createTweetElement = function (tweet) {
-  const $tweet = $('<article>').addClass('tweet-container')
-  const $header = $('<header>').append(
-    $('<img>').attr('src', tweet.user.avatars),
-    $('<h3>').addClass('tweet-owner').text(tweet.user.name),
-    $('<h4>').addClass('tweet-username').text(tweet.user.handle)
-  )
-  const $content = $('<p>').addClass('tweet-content')
-    .text(tweet.content.text)
-  const $footer = $('<footer>').append($('<p>').text(`${daysAgo(tweet.created_at)} days ago`));
-  const $icons = ['danger', 'share', 'favorite-heart-button'].map(svg => {
-    return $('<img>').attr('src', `/images/${svg}.svg`);
-  })
-  $footer.append($icons)
-  $tweet.append($header, $content, $('<hr>'), $footer);
+const createTweetElement = function(tweet) {
+  const $tweet = $("<article>").addClass("tweet-container");
+  const $header = $("<header>").append(
+    $("<img>").attr("src", tweet.user.avatars),
+    $("<h3>")
+      .addClass("tweet-owner")
+      .text(tweet.user.name),
+    $("<h4>")
+      .addClass("tweet-username")
+      .text(tweet.user.handle)
+  );
+  const $content = $("<p>")
+    .addClass("tweet-content")
+    .text(tweet.content.text);
+  const $footer = $("<footer>").append(
+    $("<p>").text(`${daysAgo(tweet.created_at)} days ago`)
+  );
+  const $icons = ["danger", "share", "favorite-heart-button"].map(svg => {
+    return $("<img>").attr("src", `/images/${svg}.svg`);
+  });
+  $footer.append($icons);
+  $tweet.append($header, $content, $("<hr>"), $footer);
   return $tweet;
-}
+};
 
 const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+  user: {
+    name: "Newton",
+    avatars: "https://i.imgur.com/73hZDYK.png",
+    handle: "@SirIsaac"
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  content: {
+    text: "If I have seen further it is by standing on the shoulders of giants"
   },
-  "created_at": 1461116232227
-}
+  created_at: 1461116232227
+};
 
-const renderTweets = (tweets) => {
-  $tweets = tweets.map(createTweetElement).reverse()
-  $('#tweets-container').empty();
-  $('#tweets article').replaceWith($tweets);
-}
+const renderTweets = tweets => {
+  $tweets = tweets.map(createTweetElement).reverse();
+  $("#tweets-container").empty();
+  $("#tweets article").replaceWith($tweets);
+};
 
-const fetchTweets = (callback) => {
-  $.getJSON('/tweets', null, (tweets) => callback(tweets))
-}
+const fetchTweets = callback => {
+  $.getJSON("/tweets", null, tweets => callback(tweets));
+};
 
 const scrollToForm = () => {
-  $('#new-tweet').slideDown();
+  $("#new-tweet").slideDown();
   window.scroll({
-    top: $('#new-tweet').offset().top,
-    behavior: 'smooth'
+    top: $("#new-tweet").offset().top,
+    behavior: "smooth"
   });
-}
+};
 
 const submitTweet = form => {
-  const data = $(form).serialize()
-  const tweetText = data.split('=')[1]
-  if(!tweetText || tweetText.length > 140) {
-    $('.error-box').slideDown()
+  const data = $(form).serialize();
+  const tweetText = data.split("=")[1];
+  if (!tweetText || tweetText.length > 140) {
+    $(".error-box").slideDown();
   } else {
-    $('.error-box').slideUp()
-    $.ajax({method: 'POST', url: '/tweets', data})
+    $(".error-box").slideUp();
+    $.ajax({ method: "POST", url: "/tweets", data })
       .done(() => {
-      $('#new-tweet textarea').val('')
-        fetchTweets(tweets => renderTweets(tweets))
-      }).fail((_, e) => console.log("Request failed:", e))
+        $("#new-tweet textarea").val("");
+        fetchTweets(tweets => renderTweets(tweets));
+      })
+      .fail((_, e) => console.log("Request failed:", e));
   }
-}
+};
 
 $(document).ready(() => {
-  $('#new-tweet').slideUp();
+  $("#new-tweet").slideUp();
 
-  $('#write-a-tweet').click(e => {
-    e.preventDefault()
-    scrollToForm()
-    $('#new-tweet textarea').focus()
-  })
-
-  fetchTweets(tweets => renderTweets(tweets))
-
-  $('#tweet-form').submit(function(e) {
+  $("#write-a-tweet").click(e => {
     e.preventDefault();
-    submitTweet(this)
-  })
+    scrollToForm();
+    $("#new-tweet textarea").focus();
+  });
+
+  fetchTweets(tweets => renderTweets(tweets));
+
+  $("#tweet-form").submit(function(e) {
+    e.preventDefault();
+    submitTweet(this);
+  });
 });
